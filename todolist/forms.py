f7b5751eed54f623b5b5494ee320affc9ae80b73
Aaltdes <aaltdes@gmail.com>
2021-02-20 15:22:57 +0300

    todolist - style: bg, center, color, icon

--- todolist/forms.py
+++ todolist/forms.py
@@ -1,9 +1,27 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Task
 
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
+
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter task...",
+            }
+        ),
+    )
+
+    completed = forms.CharField(
+        required=False,
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+            }
+        ),
+    )
